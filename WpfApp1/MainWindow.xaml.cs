d1a509835ca41aaf589f36596f9af6c1ad993dc8
mayangjie <mayhjx@foxmail.com>
2021-01-31 23:14:30 +0800

    代码优化

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -20,223 +20,203 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const double ColumnTitleHeight = 25d; // 96孔板列标题行高
+        private const double RowTitleWidth = 25d; // 96孔板行标题列宽
+        private const double CellRowHeight = 50d; // 96孔板单元格行高
+        private const double CellColWidth = 90d;  // 96孔板单元格列宽
+        private const double RowHeight = 30d; // 工作清单header和footer行高
+
         public MainWindow()
         {
             InitializeComponent();
-            Create96PlateForm("P1");
-            Create96PlateForm("P2");
-            Create96PlateForm("P3");
-            Create96PlateForm("P4");
+            PrintArea.Children.Add(Create96PlateForm());
+            PrintArea.Children.Add(Create96PlateForm());
+            PrintArea.Children.Add(Create96PlateForm());
+            PrintArea.Children.Add(Create96PlateForm());
         }
 
-
-        private void Create96PlateForm(string name)
+        private StackPanel Create96PlateForm()
         {
             var stack = new StackPanel();
-
-            stack.Children.Add(AddHeader());
-            stack.Children.Add(AddInfoHeader());
-            stack.Children.Add(CreateGrid());
-            stack.Children.Add(AddFooter());
-            PrintArea.Children.Add(stack);
-
+            stack.Children.Add(Header());
+            stack.Children.Add(InfoHeader());
+            stack.Children.Add(Body());
+            stack.Children.Add(Footer());
+            return stack;
         }
-        private Grid CreateGrid()
+
+        private void PrintButton_Click(object sender, RoutedEventArgs e)
         {
-            var grid = new Grid();
-            grid.HorizontalAlignment = HorizontalAlignment.Center;
-            grid.VerticalAlignment = VerticalAlignment.Center;
-            grid.Width = 1105;
-            grid.Height = 425;
+            PrintDialog dialog = new PrintDialog();
 
-            for (int i = 0; i < 13; i++)
+            if (dialog.ShowDialog().GetValueOrDefault() != true)
             {
-                var col = new ColumnDefinition();
+                return;
+            }
+
+            dialog.PrintVisual(PrintArea.Children[0], "print test");
+        }
+
+        private Grid Body()
+        {
+            const int rowNum = 8;
+            const int colNum = 12;
+
+            var grid = CreateGrid();
+
+            for (int i = 0; i <= colNum; i++)
+            {
                 if (i == 0)
                 {
-                    col.Width = new GridLength(25);
+                    grid.ColumnDefinitions.Add(CreateColumn(RowTitleWidth));
                 }
                 else
                 {
-                    col.Width = new GridLength(90);
+                    grid.ColumnDefinitions.Add(CreateColumn(CellColWidth));
                 }
-                grid.ColumnDefinitions.Add(col);
             }
 
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i <= rowNum; i++)
             {
-                var row = new RowDefinition();
                 if (i == 0)
                 {
-                    row.Height = new GridLength(25);
+                    grid.RowDefinitions.Add(CreateRow(ColumnTitleHeight));
                 }
                 else
                 {
-                    row.Height = new GridLength(50);
+                    grid.RowDefinitions.Add(CreateRow(CellRowHeight));
                 }
-                grid.RowDefinitions.Add(row);
             }
 
+            AddBorder(grid);
             AddColumnTitle(grid);
             AddRowTitle(grid);
-            AddBorder(grid);
             AddNumber(grid);
+
             return grid;
         }
 
-        private Grid AddHeader()
+        private Grid Header()
         {
-            var header = new Grid();
-            header.HorizontalAlignment = HorizontalAlignment.Center;
-            header.VerticalAlignment = VerticalAlignment.Center;
+            var header = CreateGrid();
 
-            var row = new RowDefinition();
-            row.Height = new GridLength(30);
-            header.RowDefinitions.Add(row);
+            header.RowDefinitions.Add(CreateRow(RowHeight));
+            header.ColumnDefinitions.Add(CreateColumn(RowTitleWidth + CellColWidth * 12));
 
-            var column = new ColumnDefinition();
-            column.Width = new GridLength(1105);
-            header.ColumnDefinitions.Add(column);
-
-            var mainTitle = new TextBlock();
-            mainTitle.Text = "前处理样品工作单（Testing Worksheet）";
-            Grid.SetRow(mainTitle, 0);
-            Grid.SetColumn(mainTitle, 0);
-            Grid.SetColumnSpan(mainTitle, 5);
-
-            header.Children.Add(mainTitle);
+            header.Children.Add(CreateTextBlock("前处理样品工作单（Testing Worksheet）", 0, 0, 1, 5));
 
             AddBorder(header);
+
             return header;
         }
 
-
-        private Grid AddInfoHeader(int plateNum = 1)
+        private Grid InfoHeader(int plateNum = 1)
         {
             // TODO: plateNum should read from excelfile
+            var infoHeader = CreateGrid();
 
-            var header = new Grid();
-            header.HorizontalAlignment = HorizontalAlignment.Center;
-            header.VerticalAlignment = VerticalAlignment.Center;
+            infoHeader.RowDefinitions.Add(CreateRow(RowHeight));
+            infoHeader.ColumnDefinitions.Add(CreateColumn(RowTitleWidth + CellColWidth));
+            infoHeader.ColumnDefinitions.Add(CreateColumn(CellColWidth * 2));
+            infoHeader.ColumnDefinitions.Add(CreateColumn(CellColWidth * 7));
+            infoHeader.ColumnDefinitions.Add(CreateColumn(CellColWidth));
+            infoHeader.ColumnDefinitions.Add(CreateColumn(CellColWidth));
 
-            var row = new RowDefinition();
-            row.Height = new GridLength(30);
-            header.RowDefinitions.Add(row);
+            infoHeader.Children.Add(CreateTextBlock("检测日期（Date)", 0, 0));
+            infoHeader.Children.Add(CreateTextBlock(DateTime.Now.ToShortDateString(), 0, 1));
+            infoHeader.Children.Add(CreateTextBlock($"板号：{plateNum} 检测仪器编号：FXS-YZ__ 系统号：__", 0, 2));
+            infoHeader.Children.Add(CreateTextBlock("板位：", 0, 3));
 
-            var column1 = new ColumnDefinition();
-            column1.Width = new GridLength(115);
-            header.ColumnDefinitions.Add(column1);
+            AddBorder(infoHeader);
 
-            var column2 = new ColumnDefinition();
-            column2.Width = new GridLength(180);
-            header.ColumnDefinitions.Add(column2);
-
-            var column3 = new ColumnDefinition();
-            column3.Width = new GridLength(630);
-            header.ColumnDefinitions.Add(column3);
-
-            var column4 = new ColumnDefinition();
-            column4.Width = new GridLength(90);
-            header.ColumnDefinitions.Add(column4);
-
-            var column5 = new ColumnDefinition();
-            column5.Width = new GridLength(90);
-            header.ColumnDefinitions.Add(column5);
-
-
-            var dateTxt = new TextBlock();
-            dateTxt.Text = "检测日期（Date)";
-            Grid.SetRow(dateTxt, 0);
-            Grid.SetColumn(dateTxt, 0);
-            header.Children.Add(dateTxt);
-
-            var date = new TextBlock();
-            date.Text = DateTime.Now.ToShortDateString();
-            Grid.SetRow(date, 0);
-            Grid.SetColumn(date, 1);
-            header.Children.Add(date);
-
-            var place = new TextBlock();
-            place.Text = $"板号：{plateNum} 检测仪器编号：FXS-YZ__ 系统号：__";
-            Grid.SetRow(place, 0);
-            Grid.SetColumn(place, 2);
-            header.Children.Add(place);
-
-            var position = new TextBlock();
-            position.Text = "板位：";
-            Grid.SetRow(position, 0);
-            Grid.SetColumn(position, 3);
-            header.Children.Add(position);
-
-            AddBorder(header);
-            return header;
+            return infoHeader;
         }
 
-
-        private Grid AddFooter()
+        private Grid Footer()
         {
-            var footer = new Grid();
-            footer.Height = 100;
-            footer.HorizontalAlignment = HorizontalAlignment.Center;
-            footer.VerticalAlignment = VerticalAlignment.Center;
+            Grid footer = CreateGrid();
+            footer.Height = 80; // 为了和下一个Grid空开
 
-            var row = new RowDefinition();
-            row.Height = new GridLength(30);
-            footer.RowDefinitions.Add(row);
+            footer.RowDefinitions.Add(CreateRow(RowHeight));
+            footer.ColumnDefinitions.Add(CreateColumn(RowTitleWidth + CellColWidth * 2));
+            footer.ColumnDefinitions.Add(CreateColumn(CellColWidth * 2));
+            footer.ColumnDefinitions.Add(CreateColumn(CellColWidth * 3));
+            footer.ColumnDefinitions.Add(CreateColumn(CellColWidth * 5));
 
-            var column1 = new ColumnDefinition();
-            column1.Width = new GridLength(205);
-            footer.ColumnDefinitions.Add(column1);
-
-            var column2 = new ColumnDefinition();
-            column2.Width = new GridLength(180);
-            footer.ColumnDefinitions.Add(column2);
-
-            var column3 = new ColumnDefinition();
-            column3.Width = new GridLength(270);
-            footer.ColumnDefinitions.Add(column3);
-
-            var column4 = new ColumnDefinition();
-            column4.Width = new GridLength(450);
-            footer.ColumnDefinitions.Add(column4);
-
-            var pipeTxt = new TextBox();
-            pipeTxt.Text = "加样枪：FXS-YY";
-            Grid.SetRow(pipeTxt, 0);
-            Grid.SetColumn(pipeTxt, 0);
-            footer.Children.Add(pipeTxt);
-
-            var operatorTxt = new TextBox();
-            operatorTxt.Text = "手工加样人员：";
-            Grid.SetRow(operatorTxt, 0);
-            Grid.SetColumn(operatorTxt, 1);
-            footer.Children.Add(operatorTxt);
-
-            var reviewTxt = new TextBox();
-            reviewTxt.Text = "加样情况审核人员：";
-            Grid.SetRow(reviewTxt, 0);
-            Grid.SetColumn(reviewTxt, 2);
-            footer.Children.Add(reviewTxt);
-
-            var remark = new TextBox();
-            remark.Text = "备注：手写的实验号必须一一核对实验号，条码和孔位";
-            Grid.SetRow(remark, 0);
-            Grid.SetColumn(remark, 3);
-            footer.Children.Add(remark);
+            footer.Children.Add(CreateTextBlock("加样枪：FXS-YY", 0, 0));
+            footer.Children.Add(CreateTextBlock("手工加样人员：", 0, 1));
+            footer.Children.Add(CreateTextBlock("加样情况审核人员：", 0, 2));
+            footer.Children.Add(CreateTextBlock("备注：手写的实验号必须一一核对实验号，条码和孔位", 0, 3));
 
             AddBorder(footer);
+
             return footer;
         }
 
+        private Grid CreateGrid()
+        {
+            return new Grid()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+        }
 
+        private RowDefinition CreateRow(double height)
+        {
+            var row = new RowDefinition();
+            row.Height = new GridLength(height);
+            return row;
+        }
+
+        private ColumnDefinition CreateColumn(double width)
+        {
+            var col = new ColumnDefinition();
+            col.Width = new GridLength(width);
+            return col;
+        }
+
+        private TextBlock CreateTextBlock(string text, int row, int column, int rowSpan = 1, int colSpan = 1)
+        {
+            var txt = new TextBlock
+            {
+                Text = text,
+                Padding = new Thickness(5)
+            };
+            Grid.SetColumn(txt, column);
+            Grid.SetRow(txt, row);
+            return txt;
+        }
+
+        private void AddBorder(Grid grid)
+        {
+            for (int row = 0; row < grid.RowDefinitions.Count; row++)
+            {
+                for (int col = 0; col < grid.ColumnDefinitions.Count; col++)
+                {
+                    var border = new Border
+                    {
+                        BorderThickness = new Thickness(1),
+                        BorderBrush = Brushes.Black
+                    };
+                    Grid.SetColumn(border, col);
+                    Grid.SetRow(border, row);
+                    grid.Children.Add(border);
+                }
+            }
+        }
+
+        #region 生成96孔板行列标题
         private void AddColumnTitle(Grid grid)
         {
             for (int col = 1; col < 13; col++)
             {
-                var t = new TextBlock();
-                t.Text = col.ToString();
-                t.FontSize = 20;
-                t.TextAlignment = TextAlignment.Center;
+                var t = new TextBlock
+                {
+                    Text = col.ToString(),
+                    FontSize = 20,
+                    TextAlignment = TextAlignment.Center
+                };
                 Grid.SetColumn(t, col);
                 Grid.SetRow(t, 0);
                 grid.Children.Add(t);
@@ -247,59 +227,30 @@ namespace WpfApp1
         {
             for (int row = 1; row < 9; row++)
             {
-                var t = new TextBlock();
-                t.Text = ((char)(row + 64)).ToString();
-                t.FontSize = 20;
-                t.TextAlignment = TextAlignment.Center;
+                var t = new TextBlock
+                {
+                    Text = ((char)(row + 64)).ToString(),
+                    FontSize = 20,
+                    TextAlignment = TextAlignment.Center
+                };
                 Grid.SetRow(t, row);
                 Grid.SetColumn(t, 0);
                 grid.Children.Add(t);
             }
         }
-
-        private void AddBorder(Grid grid)
-        {
-            for (int row = 0; row < grid.RowDefinitions.Count; row++)
-            {
-                for (int col = 0; col < grid.ColumnDefinitions.Count; col++)
-                {
-                    var border = new Border();
-                    border.BorderThickness = new Thickness(1);
-                    border.BorderBrush = Brushes.Black;
-                    Grid.SetColumn(border, col);
-                    Grid.SetRow(border, row);
-                    grid.Children.Add(border);
-                }
-            }
-        }
+        #endregion
 
         private void AddNumber(Grid grid)
         {
+            // TODO 根据移液清单中的条码和孔号生成
             for (int row = 1; row < 9; row++)
             {
                 for (int col = 1; col < 13; col++)
                 {
-                    var num = new TextBlock();
-                    num.Text = ((row - 1) * 12 + col).ToString();
-                    num.Text += " Sample";
-                    num.Text += " \n123456789";
-                    num.Padding = new Thickness(5);
-                    Grid.SetColumn(num, col);
-                    Grid.SetRow(num, row);
-                    grid.Children.Add(num);
+                    string num = ((row - 1) * 12 + col).ToString();
+                    grid.Children.Add(CreateTextBlock(num, row, col));
                 }
             }
-        }
-
-        private void PrintButton_Click(object sender, RoutedEventArgs e)
-        {
-            PrintDialog dialog = new PrintDialog();
-            if (dialog.ShowDialog().GetValueOrDefault() != true)
-            {
-                return;
-            }
-
-            dialog.PrintVisual(PrintArea.Children[0], "print test");
         }
     }
 }
